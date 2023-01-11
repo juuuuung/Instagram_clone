18dc43962e92bd0ca9ed5b0b28debe818d99305e
juuuuung <pandanam987@gmail.com>
2023-01-11 17:27:00 +0900

    footer style

--- src/components/molecules/Join/Selectview.tsx
+++ src/components/molecules/Join/Selectview.tsx
@@ -5,7 +5,7 @@ import { show_login } from "../../../store/Join";
 export default function Selectview() {
   const [show, setShow] = useAtom<boolean>(show_login);
   return (
-    <div>
+    <div className="select-view">
       <p>계정이 없으신가요?</p>
       <Button text={show ? "가입하기" : "로그인"} state={setShow} />
     </div>
