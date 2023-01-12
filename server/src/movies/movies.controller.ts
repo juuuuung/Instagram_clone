a874474b31a2b6b5c481905340d24bc4f2c30f49
juuuuung <pandanam987@gmail.com>
2023-01-12 22:06:56 +0900

    nest js 공부중

---
+++ server/src/movies/movies.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: number) {
+    return `This will return ${id} movie`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id : ${movieId}`;
+  }
+}
