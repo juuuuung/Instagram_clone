86f53d03c36cd92f63153ff55c5dab59d4fb6908
juuuuung <pandanam987@gmail.com>
2023-01-24 00:36:14 +0900

    prisma model 이름 수정됨

--- server/src/app.repository.ts
+++ server/src/app.repository.ts
@@ -1,31 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, User } from '@prisma/client';
+import { Prisma, user } from '@prisma/client';
 import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class appRepository {
   private responseMSG = {
     success: { msg: 'success' },
-    failed: { msg: 'Faild' },
+    failed: { msg: 'Failed' },
   };
   constructor(private readonly prismaService: PrismaService) {}
 
-  async findUser(userId: string): Promise<User[]> {
+  async findUser(userId: string, nickName: string = null): Promise<user[]> {
     return this.prismaService.user.findMany({
       where: {
-        // OR: [{ userId }, { nickName }],
-        userId,
+        OR: [{ userId }, { nickName }],
       },
     });
   }
 
-  async SignUp(accountData: Prisma.UserCreateInput): Promise<User> {
-    // if (await this.findUser(accountData.userId, accountData.nickName)) {
-    //   return this.responseMSG.failed;
-    // }
-    return this.prismaService.user.create({
+  async SignUp(accountData: Prisma.userCreateInput): Promise<any> {
+    const user = await this.findUser(accountData.userId, accountData.nickName);
+    if (user.length > 0) {
+      return this.responseMSG.failed;
+    }
+    await this.prismaService.user.create({
       data: { ...accountData },
     });
-    // return this.responseMSG.success;
+    return this.responseMSG.success;
   }
 }
