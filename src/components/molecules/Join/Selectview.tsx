a4c13ebced57ff671e1ddfd5cba357e4ff4050e0
juuuuung <pandanam987@gmail.com>
2023-01-29 23:54:01 +0900

    Button Component를 Login, Sign에서 사용 안함 결정

--- src/components/molecules/Join/Selectview.tsx
+++ src/components/molecules/Join/Selectview.tsx
@@ -1,13 +1,22 @@
-import Button from "../../atoms/Button";
 import { useAtom } from "jotai";
 import { show_login } from "../../../store/Join";
+import { Dispatch, SetStateAction } from "react";
+
+type Dispatcher<S> = Dispatch<SetStateAction<S>>;
 
 export default function Selectview() {
-  const [show, setShow] = useAtom<boolean>(show_login);
+  const [show, setShow]: [boolean, Dispatcher<boolean>] =
+    useAtom<boolean>(show_login);
   return (
     <div className="select-view">
       <p>계정이 없으신가요?</p>
-      <Button text={show ? "가입하기" : "로그인"} state={setShow} />
+      <button
+        onClick={() => {
+          setShow((c) => !c);
+        }}
+      >
+        {show ? "가입하기" : "로그인"}
+      </button>
     </div>
   );
 }
