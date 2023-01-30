ac885551e980d0663a72f33605683d0f246607af
juuuuung <pandanam987@gmail.com>
2023-01-31 04:13:59 +0900

    로직 분리

--- src/components/organisms/Join/Login.tsx
+++ src/components/organisms/Join/Login.tsx
@@ -1,42 +1,17 @@
-import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "../../atoms/Button";
 import Image from "../../atoms/Image";
 import "./styles/Login.scss";
 import LoginInputList from "../../molecules/Join/LoginInputList";
-import { useAtom } from "jotai";
-import { accountData, setAccountData } from "../../../store/Join";
 
 export default function Login() {
-  const [data, setData] = useAtom(accountData);
-  const onChangeFunc = (e: React.ChangeEvent<HTMLInputElement>, c: string) => {
-    let copyData = { ...data };
-    copyData[`${c}`] = e.target.value;
-    setData(copyData);
-    console.log(data);
-  };
   return (
     <div className="login">
       <div className="main-logo">
         <Image src="logo.png" />
       </div>
-      <div className="inputlist">
-        {/* <LoginInputList /> */}
-        <input
-          onChange={(e) => {
-            onChangeFunc(e, "userId");
-          }}
-          type="text"
-          placeholder="전화번호, 사용자 이름 또는 이메일"
-        />
-        <input
-          onChange={(e) => {
-            onChangeFunc(e, "userPw");
-          }}
-          type="password"
-          placeholder="비밀번호"
-        />
-      </div>
+      <LoginInputList />
+
       <div className="login-btn">
         <Button text="로그인" />
       </div>
