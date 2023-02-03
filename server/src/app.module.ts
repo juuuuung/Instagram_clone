6461d0945402ecc3efb12ad8b8a225d61ac58997
juuuuung <pandanam987@gmail.com>
2023-02-03 23:21:38 +0900

    main 화면

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
-  // providers: [JwtService],
 })
 export class AppModule {}
--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -3,6 +3,6 @@ interface props {
 }
 
 export default function Image({ src }: props) {
-  // return <img src={`img/${src}`} alt="" />;
-  return <h3>인스타</h3>;
+  return <img src={`img/${src}`} alt="" />;
+  // return <h3>인스타</h3>;
 }
